bae82f59fee3740ca162a6c967e58456bdeb2de0
rermrf <3219431643@qq.com>
2024-10-20 23:53:06 +0800

    使用prometheus监控gorm

--- internal/ioc/db.go
+++ internal/ioc/db.go
@@ -1,6 +1,7 @@
 package ioc
 
 import (
+	promsdk "github.com/prometheus/client_golang/prometheus"
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -53,11 +54,142 @@ func InitDB(l logger.LoggerV1) *gorm.DB {
 		panic(err)
 	}
 
+	// 监控查询的执行时间
+	pcb := newCallbacks()
+	pcb.registerAll(db)
+	err = db.Use(pcb)
+	if err != nil {
+		panic(err)
+	}
+
 	err = dao.InitTables(db)
 	if err != nil {
 		panic(err)
 	}
 	return db
+}
+
+type Callbacks struct {
+	vector *promsdk.SummaryVec
+}
+
+func (c *Callbacks) Name() string {
+	return "promethus-query"
+}
+
+func (c *Callbacks) Initialize(db *gorm.DB) error {
+	c.registerAll(db)
+	return nil
+}
+
+func newCallbacks() *Callbacks {
+	vector := promsdk.NewSummaryVec(promsdk.SummaryOpts{
+		// 在这边要考虑设置各种 Namespace
+		Namespace: "emoji",
+		Subsystem: "webook",
+		Name:      "gorm_quer_time",
+		Help:      "统计 GORM 的执行时间",
+		ConstLabels: map[string]string{
+			"db": "webook",
+		},
+		Objectives: map[float64]float64{
+			0.5:   0.01,
+			0.9:   0.01,
+			0.99:  0.001,
+			0.999: 0.0001,
+		},
+	},
+		// 如果是 JOIN 查询，table 就是 JOIN 在一起的
+		// 或者 table 就是主表，A JOIN B，记录的是 A
+		[]string{"type", "table"})
+
+	pcb := &Callbacks{
+		vector: vector,
+	}
+	promsdk.MustRegister(vector)
+	return pcb
+}
+
+func (c *Callbacks) registerAll(db *gorm.DB) {
+	// 作用于 INSERT 语句
+	err := db.Callback().Create().Before("*").Register("prometheus_create_before", c.before())
+	if err != nil {
+		panic(err)
+	}
+	err = db.Callback().Create().After("*").Register("prometheus_create_after", c.after("create"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Callback().Update().Before("*").Register("prometheus_update_before", c.before())
+	if err != nil {
+		panic(err)
+	}
+	err = db.Callback().Update().After("*").Register("prometheus_update_after", c.after("update"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Callback().Delete().Before("*").Register("prometheus_delete_before", c.before())
+	if err != nil {
+		panic(err)
+	}
+	err = db.Callback().Delete().After("*").Register("prometheus_delete_after", c.after("delete"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Callback().Query().Before("*").Register("prometheus_query_before", c.before())
+	if err != nil {
+		panic(err)
+	}
+	err = db.Callback().Query().After("*").Register("prometheus_query_after", c.after("query"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Callback().Row().Before("*").Register("prometheus_row_before", c.before())
+	if err != nil {
+		panic(err)
+	}
+	err = db.Callback().Row().After("*").Register("prometheus_row_after", c.after("row"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Callback().Raw().Before("*").Register("prometheus_row_before", c.before())
+	if err != nil {
+		panic(err)
+	}
+	err = db.Callback().Raw().After("*").Register("prometheus_row_after", c.after("raw"))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (c *Callbacks) before() func(db *gorm.DB) {
+	return func(db *gorm.DB) {
+		startTime := time.Now()
+		db.Set("start_time", startTime)
+	}
+}
+
+func (c *Callbacks) after(typ string) func(db *gorm.DB) {
+	return func(db *gorm.DB) {
+		val, _ := db.Get("strat_time")
+		startTime, ok := val.(time.Time)
+		if !ok {
+			// 啥都干不了
+			return
+		}
+		// 准备上报 prometheus
+
+		table := db.Statement.Table
+		if table == "" {
+			table = "unknown"
+		}
+		c.vector.WithLabelValues(typ, table).Observe(float64(time.Since(startTime).Milliseconds()))
+	}
 }
 
 type gormLoggerFunc func(msg string, fields ...logger.Field)
